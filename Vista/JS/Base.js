c1b57bdb25d6777e3b287ab65a1a46089940a123
Farmhouse <josecortijo2000@gmail.com>
2015-02-02 09:09:21 -0500

    Pagina Inicio Signed-off-by: Farmhouse <josecortijo2000@gmail.com>

--- Vista/JS/Base.js
+++ Vista/JS/Base.js
@@ -1,4 +1,4 @@
-var menu_estado=0;
+var menu_estado=1;
 var user_estado=1;
 
 function slide_menu() {
@@ -6,11 +6,15 @@ function slide_menu() {
 	if(menu_estado==0)
 	{
 		$('#Aside').css('width', '0px');
+		$('#Contenido').css('overflow', 'auto');	
+		$('#Contenido').css('height', 'auto');		
 		menu_estado=1;
 	}
 	else
 	{
 		$('#Aside').css('width', '240px');
+		$('#Contenido').css('overflow', 'hidden');	
+		$('#Contenido').css('height', 'calc(100vh - 50px)');	
 		menu_estado=0;
 
 		$('#user_menu').css('width', '0px');
@@ -24,15 +28,20 @@ function slide_user() {
 	if(user_estado==0)
 	{
 		$('#user_menu').css('width', '0px');
+		$('#Contenido').css('overflow', 'auto');	
+		$('#Contenido').css('height', 'auto');			
 		user_estado=1;
 	}
 	else
 	{
 		$('#user_menu').css('width', '240px');
+		$('#Contenido').css('overflow', 'hidden');	
+		$('#Contenido').css('height', 'calc(100vh - 50px)');		
 		user_estado=0;
 
 		$('#Aside').css('width', '0px');
 		menu_estado=1;
 	}	
+
 
 }
