d69eeaf63fefad9ce3481f57a96f5bae4c043a3c
Tayta <mgarrido.cusipaucar@gmail.com>
2015-02-07 13:11:36 -0500

    Controles
    Controles Base - SGPIP

--- Controlador/Inicio.php
+++ Controlador/Inicio.php
@@ -11,17 +11,20 @@ class ControladorInicio extends ControladorBase
         //$controllerName = str_replace("Controlador", "", get_class($this));    
     }
         
-    protected function Principal()//La accion por defecto para inicio (si no se envian parametros)
+    protected function Controles()//La accion por defecto para inicio (si no se envian parametros)
     {
         //$this->ComprobarLogin();
 
         $this->Header = file_get_contents("Vista/Secciones/Header.html");
-        $this->Contenido = file_get_contents("Vista/Contenido/Inicio.html"); 
+        $this->Contenido = file_get_contents("Vista/Contenido/Controles.html"); 
         $this->User = file_get_contents("Vista/Secciones/User.html");
         $this->Aside = file_get_contents("Vista/Secciones/Aside.html"); 
         $this->Footer = file_get_contents("Vista/Secciones/Footer.html");  
 
-        $pagina = $this->MostarElementos('',''); 
+        $jsss = array('moment','jquery.validate.min','bootstrap-datepicker','bootstrap-datetimepicker.min','autoNumeric','bootstrap3-typeahead.min','icheck.min','jquery.scrollbar.min','toastr','jstree.min','fullcalendar.min','nicEdit'); 
+        $csss = array('Controles','datepicker','bootstrap-datetimepicker.min','typeahead','iCheck-blue','jquery.scrollbar','toastr','jstree.min','fullcalendar.min');
+
+        $pagina = $this->MostarElementos($jsss, $csss); 
         print $pagina;
     }
 }
