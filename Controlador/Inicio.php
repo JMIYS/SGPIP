155daeefee052b2ed15be24d500d275227b20d9f
Tayta <mgarrido.cusipaucar@gmail.com>
2015-02-07 13:18:22 -0500

    Agregado Inicio
    Agregando Principal

--- Controlador/Inicio.php
+++ Controlador/Inicio.php
@@ -11,6 +11,20 @@ class ControladorInicio extends ControladorBase
         //$controllerName = str_replace("Controlador", "", get_class($this));    
     }
         
+    protected function Principal()//La accion por defecto para inicio (si no se envian parametros)
+    {
+        //$this->ComprobarLogin();
+
+        $this->Header = file_get_contents("Vista/Secciones/Header.html");
+        $this->Contenido = file_get_contents("Vista/Contenido/Inicio.html"); 
+        $this->User = file_get_contents("Vista/Secciones/User.html");
+        $this->Aside = file_get_contents("Vista/Secciones/Aside.html"); 
+        $this->Footer = file_get_contents("Vista/Secciones/Footer.html");  
+
+        $pagina = $this->MostarElementos('', ''); 
+        print $pagina;
+    }
+
     protected function Controles()//La accion por defecto para inicio (si no se envian parametros)
     {
         //$this->ComprobarLogin();
