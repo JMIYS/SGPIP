877938952a809a9731a55cd1d2c5256046c226c6
Farmhouse <josecortijo2000@gmail.com>
2015-02-06 20:02:24 -0500

    Ejemplos Stylos Signed-off-by: Farmhouse <josecortijo2000@gmail.com>

--- Controlador/Ejemplos.php
+++ Controlador/Ejemplos.php
@@ -24,6 +24,37 @@ class ControladorEjemplos extends ControladorBase
         $pagina = $this->MostarElementos('','Elementos'); 
         print $pagina;
     }
+
+    protected function Tablas()//La accion por defecto para inicio (si no se envian parametros)
+    {
+        //$this->ComprobarLogin();
+
+        $this->Header = file_get_contents("Vista/Secciones/Header.html");
+        $this->Contenido = file_get_contents("Vista/Contenido/Ejemplos/Tablas.html"); 
+        $this->User = file_get_contents("Vista/Secciones/User.html");
+        $this->Aside = file_get_contents("Vista/Secciones/Aside.html"); 
+        $this->Footer = file_get_contents("Vista/Secciones/Footer.html");  
+
+        $jss = array('jquery.dataTables.min', 'dataTables.responsive.min', 'Tabla_Ejemplo');
+        $csss = array('jquery.dataTables.min', 'dataTables.responsive', 'Nuevo_Tabla', 'Elementos');
+
+        $pagina = $this->MostarElementos($jss, $csss ); 
+        print $pagina;
+    }
+
+    protected function Formularios()//La accion por defecto para inicio (si no se envian parametros)
+    {
+        //$this->ComprobarLogin();
+
+        $this->Header = file_get_contents("Vista/Secciones/Header.html");
+        $this->Contenido = file_get_contents("Vista/Contenido/Ejemplos/Formularios.html"); 
+        $this->User = file_get_contents("Vista/Secciones/User.html");
+        $this->Aside = file_get_contents("Vista/Secciones/Aside.html"); 
+        $this->Footer = file_get_contents("Vista/Secciones/Footer.html");  
+
+        $pagina = $this->MostarElementos('','Elementos'); 
+        print $pagina;
+    }
 }
 
 ?>
