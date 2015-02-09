fab95f213957e7ca7613d01c85dbc4b890476d1b
irvin <irvin@hotmail.com>
2015-02-09 16:17:05 -0500

    Presupuesto v1
    inicio del presupuesto

---
+++ Controlador/Presupuesto.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once('Base.php');
+
+class ControladorPresupuesto extends ControladorBase
+{
+    //add to the parent constructor
+    public function __construct($action, $id, $urlValues) 
+    {
+        parent::__construct($action, $id, $urlValues);//llama al construc de la base controlbase
+        //$controllerName = str_replace("Controlador", "", get_class($this));    
+    }
+        
+    protected function Presupuesto()//La accion por defecto para inicio (si no se envian parametros)
+    {
+        //$this->ComprobarLogin();
+
+        $this->Header = file_get_contents("Vista/Secciones/Header.html");
+        $this->Contenido = file_get_contents("Vista/Contenido/Presupuesto/Presupuesto.html"); 
+        $this->User = file_get_contents("Vista/Secciones/User.html");
+        $this->Aside = file_get_contents("Vista/Secciones/Aside.html"); 
+        $this->Footer = file_get_contents("Vista/Secciones/Footer.html");  
+
+        $pagina = $this->MostarElementos('', 'Elementos'); 
+        print $pagina;
+    }
+
+    protected function Controles()//La accion por defecto para inicio (si no se envian parametros)
+    {
+        //$this->ComprobarLogin();
+
+        $this->Header = file_get_contents("Vista/Secciones/Header.html");
+        $this->Contenido = file_get_contents("Vista/Contenido/Controles.html"); 
+        $this->User = file_get_contents("Vista/Secciones/User.html");
+        $this->Aside = file_get_contents("Vista/Secciones/Aside.html"); 
+        $this->Footer = file_get_contents("Vista/Secciones/Footer.html");  
+
+        $jsss = array('moment','jquery.validate.min','bootstrap-datepicker','bootstrap-datetimepicker.min','autoNumeric','bootstrap3-typeahead.min','icheck.min','jquery.scrollbar.min','toastr','jstree.min','fullcalendar.min','nicEdit'); 
+        $csss = array('Controles','datepicker','bootstrap-datetimepicker.min','typeahead','iCheck-blue','jquery.scrollbar','toastr','jstree.min','fullcalendar.min');
+
+        $pagina = $this->MostarElementos($jsss, $csss); 
+        print $pagina;
+    }
+}
+
+?>
