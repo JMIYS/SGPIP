cab337ac6095efc378d81ca334ace3d9de1bcb7d
Farmhouse <josecortijo2000@gmail.com>
2015-02-13 15:47:16 -0500

    Corregio (no borrar) Signed-off-by: Farmhouse <josecortijo2000@gmail.com>

--- Modelo/Login.php
+++ Modelo/Login.php
@@ -17,7 +17,8 @@ class ModeloLogin extends ModeloBase
 			if(count($this->rows)>0)
 			{
 				$_SESSION['xafrhyjo']=array('usuario'=>$this->rows[0]['usuario'],
-									   'codigo'=>$this->rows[0]['idusuario']);
+									   'codigo'=>$this->rows[0]['idusuario'],
+									   'organismo'=>$this->rows[0]['idorganismo']);
 				return true;
 			}
 			else
@@ -39,14 +40,21 @@ class ModeloLogin extends ModeloBase
         return true;
 	}
 
+	public function ListarPrivilegios()
+	{
+	 	$this->consulta="Call spListar_Privilegios(".$_SESSION['xafrhyjo']['organismo'].")";	
+	 	$this->Consultar();
+	 	return $this->rows;
+	}
+
 	public function GetNombre()
 	{
 	 	return $_SESSION['xafrhyjo']['usuario'];
 	}
 
-	public function GetInquilino()
+	public function GetOrganismo()
 	{
-	 	return $_SESSION['xafrhyjo']['inquilino'];
+	 	return $_SESSION['xafrhyjo']['organismo'];
 	}
 
 	public function GetCodigo()
