c93b9ee42a58433f5aae6506477e1a7dec174a9b
Henry <henryslk8@gmail.com>
2015-02-26 12:48:35 -0500

    cliente

--- Modelo/Cliente.php
+++ Modelo/Cliente.php
@@ -5,14 +5,15 @@ class ModeloCliente extends ModeloBase
 {
     public function Mostrar_clientes($vars)
     {
-        $this->consulta = "SELECT * FROM tcliente WHERE idorganismo=".$vars["idorganismo"]." AND idusuario=".$vars["idusuario"].";";
+        //$this->consulta = "SELECT * FROM tcliente WHERE idorganismo=".$vars["idorganismo"]." AND idusuario=".$vars["idusuario"].";";
+        $this->consulta = "call spMostrarClientes(".$vars["idorganismo"]",".$vars["idusuario"].");";
         $this->consultar();         
         return $this->rows;       
     }   
 
     public function InsertarCliente($vars)
     {
-        $this->consulta = "INSERT INTO tcliente (razonsocial, abreviatura, descripcion, persona, ruc, dni, correo, celular, telefonofijo, direccion, paginaweb) VALUES ('".$vars["txt_razon_social"]."','".$vars["txt_abreviatura"]."','".$vars["txt_descripcion"]."','".$vars["cmb_persona"]."',".$vars["txt_ruc"].",".$vars["txt_dni"].",'".$vars["txt_correo"]."',".$vars["txt_celular"].",".$vars["txt_fijo"].",'".$vars["txt_direccion"]."','".$vars["txt_pagina"]."');";
+        $this->consulta = "call spInsertarCliente('".$vars["txt_razon_social"]."','".$vars["txt_abreviatura"]."','".$vars["txt_descripcion"]."','".$vars["cmb_persona"]."',".$vars["txt_ruc"].",".$vars["txt_dni"].",'".$vars["txt_correo"]."',".$vars["txt_celular"].",".$vars["txt_fijo"].",'".$vars["txt_direccion"]."','".$vars["txt_pagina"]."');";
         return $this->Ejecutar();        
     }   
 
